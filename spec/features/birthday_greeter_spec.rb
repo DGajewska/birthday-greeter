fe1e4972d2106e1e7bd7d1b7c01b1bd4f7ee1674
Dana Gajewska <mdgajewska@outlook.com>
2018-11-15 10:52:30 +0000

    Adds tests and code for front page

---
+++ spec/features/birthday_greeter_spec.rb
@@ -0,0 +1,28 @@
+feature 'front page' do
+
+  scenario 'greet user and ask for name' do
+    visit '/'
+    expect(page).to have_content("What's your name?")
+  end
+
+  scenario 'contain field for user to enter name' do
+    visit '/'
+    expect(page).to have_field('name')
+  end
+
+  scenario 'ask user for their birthday' do
+    visit '/'
+    expect(page).to have_content("When's your birthday?")
+  end
+
+  scenario 'contain day field for user to enter day' do
+    visit '/'
+    expect(page).to have_field('day')
+  end
+
+  scenario 'contain month field for user to enter month' do
+    visit '/'
+    expect(page).to have_field('month')
+  end
+
+end
